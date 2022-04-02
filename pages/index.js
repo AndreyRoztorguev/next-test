e6fd60ea8b4ddf0efcd62cf5e3e7b541aab04e58
Andrey Roztorguev <andrew.roztor99@gmail.com>
2022-04-02 16:43:41 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import MainContainer from "../components/MainContainer";
+
+function Index() {
+  return (
+    <MainContainer keywords="Index">
+      <div>
+        <h1>Главная страница</h1>
+      </div>
+    </MainContainer>
+  );
+}
+
+export default Index;
